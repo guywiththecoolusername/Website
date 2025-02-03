65594a31190807916423cfe9617bb59918d97f2b
guywiththecoolusername <160141722+guywiththecoolusername@users.noreply.github.com>
2025-02-03 18:59:00 +0530

    Update discord-viewer.js

--- api/discord-viewer.js
+++ api/discord-viewer.js
@@ -3,13 +3,8 @@ export default async function handler(req, res) {
 
   if (!url) {
     return res.status(400).send("❌ Error: No URL provided!");
-  }
-
-  if (!url.startsWith("https://cdn.discordapp.com/attachments/")) {
-    return res.status(400).send("❌ Error: Invalid Discord URL!");
-  }
-
-  const apiURL = `http://api.allorigins.win/get?url=${encodeURIComponent(url)}`;
+  
+  const apiURL = `https://api.allorigins.win/get?url=${encodeURIComponent(url)}`;
 
   try {
     const response = await fetch(apiURL);
